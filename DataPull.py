d26c2ce32a41b5e316255d084bfdd500341eeca3
dreyco676 <dreyco676@gmail.com>
2015-07-19 16:21:04 -0500

    basic request to the API

--- DataPull.py
+++ DataPull.py
@@ -1,2 +1,26 @@
-__author__ = 'howar_000'
-print("sam")
+import requests
+
+
+# http://www.consumerfinance.gov/complaintdatabase/technical-documentation/#more_options
+# View IDs
+# ID	CATEGORY
+# s6ew-h6mp	Consumer complaints (all categories)
+# t9fg-cqmi	Bank accounts or services
+# 7zpz-7ury	Credit cards
+# xa48-juie	Credit reporting
+# ckyu-ku28	Debt collection
+# uha4-cwwn	Money transfers
+# g5qz-smft	Mortgages
+# b239-tvpx	Other financial services
+# 6hp8-hzag	Payday loans
+# 6yuf-367p	Prepaid cards
+# eew7-9yf2	Student loans
+# wfbn-zkat	Vehicle or other consumer loans
+
+view_id = 's6ew-h6mp'
+res_type = 'json'
+request_url = 'http://data.consumerfinance.gov/api/views/' + view_id + '/rows.' + res_type
+r = requests.get(request_url)
+print(r.status_code)
+r.json()
+print(r.json)
